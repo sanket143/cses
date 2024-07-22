27e47996378923cdb68cb5b824e0bf616c981cb8
Sanket <26973649+sanket143@users.noreply.github.com>
2024-07-22 22:37:32 +0530

    2165: solved

--- src/bin/2165.rs
+++ src/bin/2165.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 #[allow(unused_imports)]
 use std::io::{stdin, stdout, BufWriter, Stdout, Write};
 
@@ -28,10 +27,6 @@ a = 1 2
 b = 1 3
 c = 2 3
 
-a = A
-b = c
-c = b
-
 2 = abc
 3 = abc a CBA c abc
 4 = (abc a CBA c abc) a (CBA C abc A CBA) c (abc a CBA c abc)
@@ -40,58 +35,31 @@ c = b
 3 = baC b Acb
 4 = abc b ACb a cbA c bac
 5 = baC b Acb a CBA C baC b (Acb c aBc A bca b cAb)
-
-2 = 3
-3 = 7
-4 = 15
-
-2 = abc
-3 = 2 a -2 c 2
-3 = 3 a -3 c 3
 */
 
-fn switch(text: String) -> String {
-    text.chars()
-        .into_iter()
-        .map(|x| {
-            if x.is_uppercase() {
-                x.to_lowercase().to_string()
-            } else {
-                x.to_uppercase().to_string()
-            }
-        })
-        .rfold(String::from(""), |mut res, x| {
-            res.push_str(x.as_str());
-            res
-        })
+fn solve_order(n: num, config: [u8; 3], result_count: &mut num, out: &mut BufWriter<Stdout>) {
+    if n == 1 {
+        writeln!(out, "{} {}", config[0], config[2]).ok();
+        return;
+    }
+
+    *result_count += 1;
+
+    solve_order(n - 1, [config[0], config[2], config[1]], result_count, out);
+    solve_order(1, config, result_count, out);
+    solve_order(n - 1, [config[1], config[0], config[2]], result_count, out);
 }
 
 fn main() {
     let out = &mut BufWriter::new(stdout());
     let mut scan = Scanner::default();
     let n = scan.next::<num>();
-    let mut hanoi_solutions = HashMap::from([(1, String::from("b")), (2, String::from("abc"))]);
+    let mut result_count: num = 0;
 
-    for i in 3..(n + 1) {
-        let mut soln = hanoi_solutions[&(i - 1)].to_owned();
-        soln.push_str("a");
-        soln.push_str(switch(hanoi_solutions[&(i - 1)].to_owned()).as_str());
-        soln.push_str("c");
-        soln.push_str(hanoi_solutions[&(i - 1)].to_owned().as_str());
-        hanoi_solutions.insert(i, soln);
-        hanoi_solutions.remove(&(i - 1));
+    for _ in 0..n {
+        result_count += result_count + 1;
     }
 
-    writeln!(out, "{}", hanoi_solutions[&n].len()).ok();
-    for i in hanoi_solutions[&n].chars() {
-        match i {
-            'a' => writeln!(out, "1 2").ok(),
-            'b' => writeln!(out, "1 3").ok(),
-            'c' => writeln!(out, "2 3").ok(),
-            'A' => writeln!(out, "2 1").ok(),
-            'B' => writeln!(out, "3 1").ok(),
-            'C' => writeln!(out, "3 2").ok(),
-            _ => writeln!(out, "").ok(),
-        };
-    }
+    writeln!(out, "{result_count}").ok();
+    solve_order(n, [1, 2, 3], &mut result_count, out);
 }
